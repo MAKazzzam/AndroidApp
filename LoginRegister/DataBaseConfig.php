312863585fc245ce2155a997a496173a295db40e
MAKazzzam <85233852+MAKazzzam@users.noreply.github.com>
2021-12-19 18:48:33 +0200

    Add files via upload

---
+++ LoginRegister/DataBaseConfig.php
@@ -0,0 +1,22 @@
+<?php
+
+class DataBaseConfig
+{
+    public $servername;
+    public $username;
+    public $password;
+    public $databasename;
+
+    public function __construct()
+    {
+
+        $this->servername = 'sql11.freemysqlhosting.net';
+        $this->username = 'sql11459976';
+        $this->password = 'AfK5YsGpyw';
+        $this->databasename = 'sql11459976';
+
+
+    }
+}
+
+?>
